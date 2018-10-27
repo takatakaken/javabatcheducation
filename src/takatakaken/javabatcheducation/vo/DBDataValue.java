9ee2c705e7f10aa52720d76c2951f234cac94006
takatakaken <tetteketetake@gmail.com>
2018-10-27 20:46:19 +0900

    VO内記載を付けるように変更

--- src/takatakaken/javabatcheducation/vo/DBDataValue.java
+++ src/takatakaken/javabatcheducation/vo/DBDataValue.java
@@ -1,5 +1,18 @@
 package takatakaken.javabatcheducation.vo;
 
+/**
+ * 仮で実装したDBDataValue<br>
+ *
+ * 目的に合わせて型や名称は好きなように変えてください。
+ *
+ */
 public class DBDataValue {
 
+	private String aaa;
+
+	private String bbb;
+
+	private String ccc;
+
+	private String ddd;
 }
